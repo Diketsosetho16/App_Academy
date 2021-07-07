ab8b2d67cae45ae11cb3ab425ccb5075308b8784
Diketso Setho <45754364+Diketsosetho16@users.noreply.github.com>
2021-07-08 00:45:47 +0200

    Splash screen

---
+++ app/src/main/java/e/com/rep/MainActivity.kt
@@ -0,0 +1,14 @@
+package e.com.rep
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Toast.makeText(this,"Welcome",Toast.LENGTH_SHORT).show()
+    }
+}
